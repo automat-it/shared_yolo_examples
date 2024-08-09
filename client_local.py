1ef20b0760d2c0cfdfc5401e327f9126235182c7
Oleg Iurchenko <46219907+olegy-ait@users.noreply.github.com>
2024-08-09 14:25:16 +0300

    Update client_local.py

--- client_local.py
+++ client_local.py
@@ -1,30 +1,25 @@
-import numpy as np 
-import base64
-import zlib
-import time
 import cv2
-import pickle
 from ultralytics import YOLO
-import pickle
 import time
+import sys
 
-
+sleep_time=float(sys.argv[1])
 model_sample_model = YOLO("./models/sample_model/yolov8n.pt", task="detect")
 
 #for _ in range(1000):
-image = cv2.imread("/tmp/test.jpg")
+image = cv2.imread("026-dvd2_star_wars_poster_classic_1_web.jpg")
 image = cv2.resize(image, (640, 640))
-for i in range(1,5):
-	print(f'RUN {i} -----------------------------------------------------------------')
-	start = time.time_ns()
-	tracks = model_sample_model.track(
-	            image,
-	            conf=0.5,
-	            persist=True,
-	            tracker="bytetrack.yaml",
-	            show=False,
-	            verbose=True,
-	    )
-	end = time.time_ns()
-	print(f'Start: {start}, End: {end}, Exec time:{(end-start)/1000000} ms')
-
+while True:
+    print(f'RUN  -----------------------------------------------------------------')
+    start = time.time_ns()
+    tracks = model_sample_model.track(
+                image,
+                conf=0.5,
+                persist=True,
+                tracker="bytetrack.yaml",
+                show=False,
+                verbose=True,
+        )
+    end = time.time_ns()
+    print(f'Start: {start}, End: {end}, Exec time:{(end-start)/1000000} ms')
+    time.sleep(sleep_time)
